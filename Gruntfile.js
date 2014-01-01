b87b0616135dfab2c6b3e1ef5be75d5b573d8917
pc035860 <pc035860@gmail.com>
2014-01-01 23:52:24 +0800

    Setup project basics

---
+++ Gruntfile.js
@@ -0,0 +1,56 @@
+/*global module*/
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc'
+      },
+      src: ['src/*.js'],
+      gruntfile: ['Gruntfile.js']
+    },
+    uglify: {
+      build: {
+        src: '<%= pkg.name %>.js',
+        dest: '<%= pkg.name %>.min.js'
+      },
+      options: {
+        mangle: true,
+        compress: true,
+        banner: 
+          '/*! <%= pkg.name %>\n' + 
+          'version: <%= pkg.version %>\n' +
+          'build date: <%= grunt.template.today("yyyy-mm-dd") %>\n' + 
+          'author: <%= pkg.author %>\n' + 
+          '<%= pkg.repository.url %> */\n'
+      }
+    },
+    watch: {
+      gruntfile: {
+        files: 'Gruntfile.js',
+        tasks: ['jshint:gruntfile']
+      },
+      src: {
+        files: '<%= pkg.name %>.js',
+        tasks: ['default']
+      }
+    },
+    connect: {
+      server: {
+        options: {
+          port: 8080,
+          base: '',
+          keepalive: true
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+
+  grunt.registerTask('default', ['jshint:beforeuglify', 'uglify']);
+};
+
